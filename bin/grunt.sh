056557bc422802ff99b7feb8943ead055805db46
Ryan VanGundy <ryanvangundy@Ryans-MacBook-Pro.local>
2013-12-19 02:20:02 -0500

    Testing with install grunt & grunt-cli earlier

--- bin/grunt.sh
+++ bin/grunt.sh
@@ -1,8 +1,5 @@
 run_grunt() {
   if [ -f $build_dir/grunt.js ] || [ -f $build_dir/Gruntfile.js ] || [ -f $build_dir/Gruntfile.coffee ]; then
-    # make sure that grunt and grunt-cli are installed locally
-    npm install grunt-cli --save-dev
-    npm install grunt --save-dev
     status "Found Gruntfile, running grunt heroku task"
     $build_dir/node_modules/.bin/grunt heroku
   else
